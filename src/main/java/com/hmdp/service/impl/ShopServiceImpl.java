a1300cc63e8cf66ce6fdfb19f0fad859f29cc4b5
ajie <1651588782@qq.com>
2022-10-21 20:19:42 +0800

    缓存""值解决缓存穿透问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -13,8 +13,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -41,9 +40,17 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
+        //判断命中的值是否为空 ---  set(key, "")
+        if ("".equals(shopJson)) {
+            // 返回错误信息
+            return Result.fail("店铺信息不存在");
+        }
         //4、不存在，根据id查询数据库
         Shop shop = this.getById(id);
         if (shop == null) {
+            //将空值写入redis
+            stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+
             //5、不存在，返回错误
             return Result.fail("店铺不存在！");
         }
