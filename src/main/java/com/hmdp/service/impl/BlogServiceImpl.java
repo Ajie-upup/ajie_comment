d0ee8bc8e1da54e158f341a0822d8ba678e25e8b
ajie <1651588782@qq.com>
2022-10-23 14:34:17 +0800

    实现达人探店发布博客功能，实现点赞功能

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,14 +1,27 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.SystemConstants;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.BLOG_LIKED_KEY;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -16,5 +29,97 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
+    @Resource
+    private IUserService userService;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryHotBlog(Integer current) {
+        // 根据用户查询
+        Page<Blog> page = query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(blog -> {
+            this.queryBlogUser(blog);
+            this.isBlogLiked(blog);
+        });
+        return Result.ok(records);
+    }
+
+
+    @Override
+    public Result queryBlogById(Long id) {
+        //1、查询blog
+        Blog blog = this.getById(id);
+        if (blog == null) {
+            return Result.fail("blog不存在");
+        }
+        //2、查询blog有关的用户
+        this.queryBlogUser(blog);
+        //3、查询blog是否被点赞
+        this.isBlogLiked(blog);
+        return Result.ok(blog);
+    }
+
+    private void isBlogLiked(Blog blog) {
+        //1、获取用户
+        Long userId = UserHolder.getUser().getId();
+        //2、判断当前用户是否已经点赞
+        String key = BLOG_LIKED_KEY + blog.getId();
+        Boolean isMember = stringRedisTemplate.opsForSet().isMember(key, userId.toString());
+        blog.setIsLike(BooleanUtil.isTrue(isMember));
+    }
+
+    /**
+     * 根据blog查询用户,将用户信息封装到blog中的字段中
+     *
+     * @param blog
+     */
+    private void queryBlogUser(Blog blog) {
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+    }
+
+    @Override
+    public Result likeBlog(Long id) {
+        //1、获取用户
+        Long userId = UserHolder.getUser().getId();
+        //2、判断当前用户是否已经点赞
+        String key = BLOG_LIKED_KEY + id;
+        Boolean isMember = stringRedisTemplate.opsForSet().isMember(key, userId.toString());
+
+        if (BooleanUtil.isFalse(isMember)) {
+            //3、如果未点赞，可以点赞
+            boolean isSuccess = this.update()
+                    //3.1、数据库点赞数+1
+                    .setSql("liked = liked + 1")
+                    .eq("id", id)
+                    .update();
+            //3.2、保存用户到redis的set集合中
+            if (isSuccess) {
+                stringRedisTemplate.opsForSet().add(key, userId.toString());
+            }
+        } else {
+            //4、如果已点赞，可以取消点赞
+            boolean isSuccess = this.update()
+                    //4.1、数据库点赞数-1
+                    .setSql("liked = liked - 1")
+                    .eq("id", id)
+                    .update();
+            //4.2、将用户从redis的set集合中删除
+            if (isSuccess) {
+                stringRedisTemplate.opsForSet().remove(key, userId.toString());
+            }
+        }
+        return Result.ok();
+    }
+
 
 }
